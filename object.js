ee1a971307cd65b11f51969ec02bef0e476942ff
Michael Kronk <mkronk7@gmail.com>
2015-04-06 14:55:12 -0400

    early vs object literal notation

--- object.js
+++ object.js
@@ -1,1 +1,20 @@
-//Let's start here.
+//In ECMA-262, objects are defined as unordered arrays
+
+//Early object notation
+var beer = new Object();
+beer.name = "PaleFish";
+beer.style = "IPA";
+
+beer.sayName = function() {
+	alert(this.name);
+}
+
+//Object literal notation
+var beer = {
+	name: "PaleFish";
+	style: "IPA";
+
+	sayName: function() {
+		alert(this.name);
+	}
+};
