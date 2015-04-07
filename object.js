23b3fb0e0409017f386e4803b34e3b633df805a1
Michael Kronk <mkronk7@gmail.com>
2015-04-07 10:20:11 -0400

    factory pattern

--- object.js
+++ object.js
@@ -1,6 +1,6 @@
 //In ECMA-262, objects are defined as unordered arrays
 
-//Early object notation
+//Early object notation with the Object constructor
 var beer = new Object();
 beer.name = "PaleFish";
 beer.style = "IPA";
@@ -18,3 +18,17 @@ var beer = {
 		alert(this.name);
 	}
 };
+
+//Factory pattern, to save time with creating multiple objects using the same interface
+function createBeer(name, style){
+	var b = new Object();
+	b.name = name;
+	b.style = style;
+	b.sayName = function() {
+		alert(this.name);
+	};
+	return b;
+}
+
+var beer1 = createBeer("Castle", "Stout");
+var beer2 = createBeer("Surf", "Lager");
