69309140c713d494736ba209b7e97a94e9309817
Michael Kronk <mkronk7@gmail.com>
2015-08-01 07:51:41 -0400

    Update object.js with prototype-based inheritance and prototype chain mechanism notes via CodeHub on iPhone

--- object.js
+++ object.js
@@ -33,3 +33,75 @@ function createBeer(name, style){
 var beer1 = createBeer("Castle", "Stout");
 var beer2 = createBeer("Surf", "Lager");
 
+// Prototypes determine properties and methods that associated objects inherit from
+
+//Every object holds a prototype property - add properties and methods to this for inheritance
+
+function CraftBeer(beerName) {
+    this.name = beerName;
+}
+
+CraftBeer.prototype.printName = function() {
+    console.log(this.name);
+}
+
+var newBeer = new CraftBeer("Robust Ale");
+
+newBeer.printName();
+
+//Every object also holds a prototype attribute/prototype object which indicates the object that provides the inherited properties and methods
+
+//Objects created with object literals have their prototype attribute set to Object.prototype
+
+var beach = {name: "Sanibel"};
+console.log(beach.constructor);
+console.log(beach.prototype);
+
+//Objects created with constructors have their prototype attribute set to the constructor
+
+function Island(islandName) {
+    this.name = islandName;
+}
+
+var island = new Island("Sanibel Island");
+console.log(island.constructor);
+console.log(island.prototype);
+
+//In JS, prototype-based inheritance is implemented with the prototype property
+
+function Gym () {
+    this.city = "Chapel Hill";
+    this.hasSquatRack = true;
+}
+
+Gym.prototype.showNameAndPrice = function() {
+    console.log("This is the " + this.Name + " gym and it costs " + this.Price + " a month.";
+}
+
+function champGym (gymName, gymPrice) {
+    this.name = gymName;
+    this.price = gymPrice;
+}
+
+champGym.prototype = new Gym();
+
+var aGym = new champGym("Champion's Gym", "$50");
+
+//Print name from aGym prototype, which is champGym.prototype
+
+console.log(aGym.name);
+
+//Use method from champGym prototype, which is Gym.prototype. This demonstrates the prototype chain, which extends from aGym to champGym to Gym.
+
+aGym.showNameAndPrice;
+
+//Regarding the prototype chain mechanism, the JS runtime searches for an object's property first on the immediate object. If the property isn't found, the search moves up to the object's prototype and continues up the prototype chain until either the property is found or it isn't (in which case undefined is returned)
+
+//All objects in JS inherit properties and methods from Object.prototype
+
+
+
+
+
+
+
