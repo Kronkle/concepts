b7ca27d83297bc672177d240b6bd9b812450ab1a
Kronkle <mkronk7@gmail.com>
2015-08-06 12:44:31 -0400

    jasmine tutorial files added

---
+++ jasmine_tutorial.js
@@ -0,0 +1,15 @@
+// Jasmine is a BDD testing framework for JS that runs independently of any browsers, JS framework, and the DOM
+
+// This is a Jasmine source file, which contains defintions for functions that will be tested by the associated Jasmine spec file
+var n = 3;
+var m = 4;
+
+function addSomething(n, m) {
+	return n + m;
+};
+
+function stateName() {
+	return {
+		name: "Mike"
+	};
+};
