0209151133f31a1ce4592c7af24cff266db27f91
Michael Kronk <mkronk7@gmail.com>
2015-09-30 18:27:52 -0400

    finished writing gulp tutorial code

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,11 +1,43 @@
 // Include gulp
 var gulp = require('gulp');
 
-// Include our plugins
+// Include plugins that gulp will use
 var jshint = require('gulp-jshint'),
     sass = require('gulp-sass'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename');
 
+// Run JavaScript through JSHint linting tool
+gulp.task('lint', function() {
+	return gulp.src('js/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+});
+
+// Compile Sass files into CSS
+gulp.task('sass', function() {
+	return gulp.src('scss/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('css'));
+});
+
+// Concatenate and minify JavaScript
+gulp.task('scripts', function() {
+	return gulp.src('js/*.js')
+		.pipe(concat('all.js'))
+		.pipe(gulp.dest('dist'))
+		.pipe(rename('all.min.js'))
+		.pipe(uglify())
+		.pipe(gulp.dest('dist'));
+});
+
+// Watch files for change
+gulp.task('watch', function() {
+	gulp.watch('js/*.js', ['lint', 'scripts']);
+	gulp.watch('scss/*.scss', ['sass']);
+});
+
+// Run default task which serves as a wrapper for the preceding tasks
+gulp.task('default', ['lint', 'sass', 'scripts', 'watch']);
 
