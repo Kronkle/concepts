624fa6985ce9b7a3a24206d0adb88b42926650b0
Michael Kronk <mkronk7@gmail.com>
2015-10-20 08:00:52 -0400

    started algorithms directory for practice with algorithms, started binary_search.js

---
+++ algorithms/binary_search.js
@@ -0,0 +1,17 @@
+/*
+ * Binary search finds the position of a target value within a sorted array 
+ * within O(log n) time. With every number found, go either higher or lower using
+ * a divide-and-conquer strategy.
+ */
+
+// Create an array of numbers
+var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// Create binary search function
+var binarySearch = function(array, num, first, last) {
+	
+	// Calculate middle value within this particular subset 
+	var mid = Math.floor(( first + last ) / 2);
+
+};
+
