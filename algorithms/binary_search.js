5d9d597b0226d4aa81f17db4a0ed5ebbfa957eb1
Michael Kronk <mkronk7@gmail.com>
2015-10-22 19:32:30 -0400

    finished binary_search.js example

--- algorithms/binary_search.js
+++ algorithms/binary_search.js
@@ -5,13 +5,30 @@
  */
 
 // Create an array of numbers
-var nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 // Create binary search function
-var binarySearch = function(array, num, first, last) {
+var binarySearch = function(numbers, target, first, last) {
 	
-	// Calculate middle value within this particular subset 
+	// Calculate middle position within this particular array
 	var mid = Math.floor(( first + last ) / 2);
+
+	// Retrieve value from middle position
+	var val = numbers[mid];
+
+	// Run a binary search on first half of array if value in middle position is greater than target
+	if (val > target) {
+		return binarySearch(numbers, target, first, mid-1);
+	}
+
+	// Run a binary search on second half of array if value in middle position is less than target
+	if (val < target) {
+		return binarySearch(numbers, target, mid+1, end);
+	}
+
+	// Return middle position if value isn't less than or greater than target (found!)
+	return mid;
 
 };
 
+console.log(binarySearch(numbers, 5, 0, numbers.length - 1));
