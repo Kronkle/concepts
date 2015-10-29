a23b178c2bf4c7757544caf1529866183212400d
Michael Kronk <mkronk7@gmail.com>
2015-10-29 19:58:47 -0400

    more code for quicksort tutorial

--- algorithms/quicksort.js
+++ algorithms/quicksort.js
@@ -14,3 +14,30 @@ var swap = new function(items, firstIndex, secondIndex) {
 	items[firstIndex] = items[secondIndex];
 	items[secondIndex] = temp;
 };
+
+
+var partition = new function(items, left, right) {
+
+	var pivot = items[Math.floor((right+left) / 2)],
+			i = left,
+			j = right;
+
+	while (i <= j) {
+		
+		while(items[i] < pivot){
+			i++;
+		}
+
+		while(items[j] > pivot) {
+			j--;
+		}
+
+		if (i <= j){
+			swap(items, i, j);
+			i++;
+			j--;
+		}
+	}
+
+	return i;
+}
